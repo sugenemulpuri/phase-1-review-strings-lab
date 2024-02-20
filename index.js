da019c79320c2d9310fd46f268011dfe93a9e5f7
Sugene Mulpuri <sugenemulpuri9@gmail.com>
2024-02-20 12:55:01 -0600

    phase-1-review-strings-lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+
+const currentUser = 'Sugene Mulpuri'
+
+const welcomeMessage = `Welcome to Flatbook,  + ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
